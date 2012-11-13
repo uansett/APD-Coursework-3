54df19590054de07dee8e5f2c1e4746842e249f8
uansett <stian.uansett@gmail.com>
2012-11-13 12:13:24 +0000

    Fixed some characters in the output

--- APD Coursework 3/src/johansen/stian/apd/cw3/Kennel.java
+++ APD Coursework 3/src/johansen/stian/apd/cw3/Kennel.java
@@ -8,27 +8,27 @@ public class Kennel {
         DogBot mrBean = new DogBot("Mr. Bean",6,3);
         
         DogWatcher harold = new DogWatcher(fluffy, "Harold");
-        DogWatcher haroldsSon = new DogWatcher(mrBean, "Harold`s Son");
+        DogWatcher haroldsSon = new DogWatcher(mrBean, "Harold's Son");
         DogWatcher steven = new DogWatcher(princeHarry, "Steven");
         
         System.out.println( "--- Initial: Fluffy is watched over by "
                             + "Harold."
                             + "\nPrince Harry is being watched by Steven."
-                            + "\nMr. Bean is being watched by Harolds Son.");
+                            + "\nMr. Bean is being watched by Harold's Son.");
         mrBean.play();
         fluffy.play();
         princeHarry.play();
         
         fluffy.addObserver(haroldsSon);
         System.out.println("Now, both Mr bean and Fluffy is watched over by "
-                            + "Harolds Son.");
+                            + "Harold's Son.");
         
         mrBean.eat();
         fluffy.eat();
         princeHarry.eat();
         
         mrBean.deleteObserver(haroldsSon);
-        System.out.println( "Now, Harolds Son lost Mr Bean out of sight "
+        System.out.println( "Now, Harold's Son lost Mr Bean out of sight "
                             + "and is only watching fluffy together "
                             + "with Harold.");
         
